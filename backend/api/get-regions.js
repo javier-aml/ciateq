664931ef101716214c4f0c605cb5802402a476c2
javier-aml <80180853+javier-aml@users.noreply.github.com>
2021-03-08 13:22:00 -0600

    Create get-regions.js

---
+++ backend/api/get-regions.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+module.exports = (context,req) => {
+    let results = null;
+	const config = {
+		server:'ciateq.database.windows.net',
+		database:'CIATEQ',
+		user:'javier_aml',
+		password:'Altamirano302',
+		port:1433,
+		options:{
+			encrypt: true
+		}
+	};
+	sql.connect(config,error => {
+        const request = new sql.Request();
+        request.execute('spGetRegions',(error,res) => {
+            results = res.recordset;
+            context.res = {
+                body:{response:results},
+                headers:{'Content-Type':'application/json'}
+            };
+            context.done();
+        });
+    });
+}
